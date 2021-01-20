b4ebe5297112c3afd8f48af8c097314847cc4c62
evannor <erica.vannorman@gmail.com>
2021-01-19 17:48:32 -0800

    Initializing use of state

--- src/App.js
+++ src/App.js
@@ -3,16 +3,34 @@ import "./App.css";
 import Person from "./Person/Person";
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Erica", age: 32 },
+      { name: "Fred", age: 24 },
+      { name: "Garcia", age: 45 },
+    ],
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Hello, World!</h1>
         <p>This is really working!</p>
-        <Person name="Erica" age="32" />
-        <Person name="Fred" age="24">
+        <button>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        >
           My Hobbies: Juggling
         </Person>
-        <Person name="Garcia" age="45" />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
       </div>
     );
   }
