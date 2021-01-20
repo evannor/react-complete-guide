8385e3916466c9a172215d64df7401e529f19251
evannor <erica.vannorman@gmail.com>
2021-01-19 17:35:59 -0800

    children props

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,9 +2,13 @@ import React from "react";
 
 const person = (props) => {
   return (
-    <p>
-      Howdy! My name is {props.name} and I am {props.age} years old.
-    </p>
+    <div>
+      <p>
+        Howdy! My name is {props.name} and I am {props.age} years old.
+      </p>
+      {/* Passing text from wrapped Person text */}
+      <p>{props.children}</p>
+    </div>
   );
 };
 
