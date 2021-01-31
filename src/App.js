629569cd67ae63ce91dce081418c2eff61782dd2
evannor <erica.vannorman@gmail.com>
2021-01-31 15:13:42 -0800

    Set dynamic classes

--- src/App.js
+++ src/App.js
@@ -85,10 +85,19 @@ class App extends Component {
       style.backgroundColor = "red";
     }
 
+    // dyanmically set class
+    let classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push("red");
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push("bold");
+    }
+
     return (
       <div className="App">
         <h1>Hello, World!</h1>
-        <p>This is really working!</p>
+        <p className={classes.join(" ")}>This is really working!</p>
         <button style={style} onClick={this.togglePersonsHandler}>
           Show Person Components
         </button>
